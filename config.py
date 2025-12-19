1818c1fb341e31af5187952a60d881264ef64584
Levhcwm21. <alexags1983@gmail.com>
2025-12-19 21:49:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "27394279")
-    API_HASH  = os.environ.get("API_HASH", "90a9aa4c31afa3750da5fd686c410851")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7721902522:AAGDZP9Qn6CpP-Uc87TWdtPyYvWX4NXuCls") 
+    API_ID    = os.environ.get("API_ID", "23340285")
+    API_HASH  = os.environ.get("API_HASH", "ab18f905cb5f4a75d41bb48d20acfa50")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8500246274:AAETH_r_w1R5AUOj_oLyQuLYbxJK20PBm68") 
 
     # database config
     DB_NAME = os.environ.get("DB_NAME","dmo")     
@@ -18,12 +18,12 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/29a3acbbab9de5f45a5fe.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7465574522').split()]
-    FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'The_TGguy').split(',')
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002288135729"))
-    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002288135729"))
+    FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'Stores_Universe').split(',')
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003360643497"))
+    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1003360643497"))
     
     # wes response configuration     
-    WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
+    WEBHOOK = bool(os.environ.get("WEBHOOK", "False"))
 
 
 class Txt(object):
