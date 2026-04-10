b2f469a34fc50fecb1b0db9fb1cbc1e638c0b4de
Levhcwm21. <alexags1983@gmail.com>
2026-04-10 18:39:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME","dmo")     
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://python21java:8ZFGYMKJCqAPwsiO@filestore.f876hjv.mongodb.net/?retryWrites=true&w=majority&appName=Filestore")
+    DB_NAME = os.environ.get("DB_NAME","")     
+    DB_URL  = os.environ.get("DB_URL","")
     PORT = os.environ.get("PORT", "8080")
  
     # other configs
