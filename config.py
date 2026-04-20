6565ae2ded584f3fb481bd0f98f9d40f9602e6d1
Levhcwm21. <alexags1983@gmail.com>
2026-04-20 11:37:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config(object):
 
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/29a3acbbab9de5f45a5fe.jpg")
+    START_PIC   = os.environ.get("START_PIC", "https://files.catbox.moe/lllex3.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7465574522').split()]
     FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'The_TGguy').split(',')
     LOG_CHANNEL  = int(os.environ.get("LOG_CHANNEL",  "-1002288135729"))
